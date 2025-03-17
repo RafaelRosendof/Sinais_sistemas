10560f8c4452c00639b6357a0ed3d702281995c7
Rafael <rafael.19.rosendo@gmail.com>
2025-03-17 14:04:26 -0300

    início do projeto open source

---
+++ cppPart/methods.hpp
@@ -0,0 +1,65 @@
+#ifndef METHODS_H
+#define METHODS_H
+
+
+#include <iostream>
+//Adicionar métodos de boost para numeros de precisão arbitrária
+
+
+class Methods {
+    
+
+
+
+    /*
+
+    MÉTODOS A FAZER 
+
+
+    todos os métodos de números complexos{
+    
+    adição 
+    subtração 
+    multiplicalçao
+    divisão
+    norma 
+    conjugado
+    }    
+
+    Métodos de matrizes e sistemas lineares{
+
+    adição de matrizes 
+    multiplicação matrizes 
+
+    decomposição LU 
+
+    norma de vetor 
+
+    por enquanto só
+
+    }
+
+    agora parte de sinais e sistemas 
+
+    transformada de fourier
+    e métodos adjacentes 
+
+    buscar biblioteca para visualizar dados em tempo real e animações posso usar uma api de python para issso
+
+
+
+    TODO:    posturmamente 
+    transformada de laplace
+    e métodos adjacentes
+
+    transformada de z
+    e métodos adjacentes
+
+
+    */
+};
+
+#endif // METHODS_H
+
+
+
